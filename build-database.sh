78eb43d79bb845186445e9c3d6adbf20d272ae3f
Demir Yerli <mrquantumoff@protonmail.com>
2022-05-07 09:37:25 +0300

    fixes

--- build-database.sh
+++ build-database.sh
@@ -7,13 +7,13 @@ repo-add -s -n -R x86_64/bultek.db.tar.gz  x86_64/*.pkg.tar.zst
 
 # Removing the symlinks because GitHub can't handle them.
 rm x86_64/bultek.db
-#rm x86_64/bultek.db.sig
+rm x86_64/bultek.db.sig
 rm x86_64/bultek.files
-#rm x86_64/bultek.files.sig
+rm x86_64/bultek.files.sig
 
 # Copy files
 cp x86_64/bultek.db.tar.gz x86_64/bultek.db
-#cp x86_64/bultek.db.sig.tar.gz x86_64/bultek.db.sig
+cp x86_64/bultek.db.tar.gz.sig x86_64/bultek.db.sig
 cp x86_64/bultek.files.tar.gz x86_64/bultek.files
-#cp x86_64/bultek.files.sig.tar.gz x86_64/bultek.files.sig
+cp x86_64/bultek.files.tar.gz.sig x86_64/bultek.files.sig
 
